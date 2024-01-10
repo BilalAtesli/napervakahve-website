9d5cc74567d02a5160fbc2545a4af08a4c4b735a
Bilal Atesli <ateslibilal@gmail.com>
2024-01-10 17:58:23 +0300

    update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const user = "admin";
+const password = "admin";
+
+const buttonElement = document.querySelector("#login");
+const warningElement = document.querySelector("#warning");
+buttonElement.addEventListener("click", (event) => {
+  event.preventDefault();
+  const enteredUsername = document.querySelector("#username").value;
+  const enteredPassword = document.querySelector("#password").value;
+  if (enteredUsername === user && enteredPassword === password) {
+    window.location.href = "index.html";
+  } else {
+    warningElement.classList.add("warning");
+    warningElement.innerHTML = "<p>Şifre veya Kullanıcı adı yanlış !</p>";
+  }
+});
